c23ff06b02e977b7674366a5ff65ef9aa6befc2a
Sho Maeda <syou20020911@gmail.com>
2022-08-13 14:37:24 +0900

    fix

--- src/content.php
+++ src/content.php
@@ -1,24 +1,18 @@
 <div class="container mx-auto">
   <div class="flex flex-col text-white">
-    <div class="">
+    <div class="flex flex-col">
       <div class="text-xl">Sho Maeda</div>
-      <div class="text-xs">university student</div>
-    </div>
-    <!-- <div> -->
-      <!-- <div>picture</div> -->
-    <!-- </div> -->
-    <div class="pt-4">
-      <div>
-        <div class="border-b-4 border-gray-500"><div>Work</div></div>
-        <div>Usually, I walk around on Github to use a lot of libraries for development.</div>
-      </div>
+      <div class="text-xs">University Student / Enthusiastic Web Developer</div>
     </div>
     <div class="pt-4">
-      <div class="border-b-4 border-gray-500"><div class="">Bio</div></div class="flex flex-col mx-auto">
-        <div><div class="inline-block text-bold pr-2 pt-2">2002</div>Born in Saitama, Japan.</div>
-        <div><div class="inline-block text-bold pr-2">2021</div>Get into Chuo University.</div>
-        <div><div class="inline-block text-bold pr-2"><?= date("Y");?></div>Now ...</div>
-      </div>
+        <div class="inline-block border-b-4 border-gray-500 text-bold">Work</div>
+        <div>Usually, I walk around on GitHub to use a lot of libraries for development.</div>
+    </div>
+    <div class="pt-4">
+      <div class="inline-block border-b-4 border-gray-500 text-blod">Bio</div>
+      <div><div class="inline-block text-bold pr-2 pt-2">2002</div>Born in Saitama, Japan.</div>
+      <div><div class="inline-block text-bold pr-2">2021</div>Get into Chuo University.</div>
+      <div><div class="inline-block text-bold pr-2"><?= date("Y");?></div>Now ...</div>
     </div>
   </div>
 </div>
