3fe2c0b8f5e83c9963c7fec60e976a4e71556630
Sho Maeda <syou20020911@gmail.com>
2022-08-13 10:59:57 +0900

    fix

--- src/header.php
+++ src/header.php
@@ -1,16 +1,17 @@
 <header>
   <nav class="container flex flex-wrap justify-between items-center mx-auto">
-    <div class="text-bold text-white">Maeda Sho</div>
+    <div class="">
+      <div class="text-bold text-white">Maeda Sho</div>
+    </div>
     <div></div>
     <div></div>
     <div></div>
     <div class="flex flex-wrap justify-between items-center ">
-      <div class="p-4 pr-4 text-bold text-white">
-      About me
-      <div class="bg-gray-100 opacity-10 rounded-lg"></div>
+      <div class="p-4 pr-4 m-2 bg-gray-300 opacity-20 rounded-lg">
+        <div class="text-bold text-gray">About me</div>
       </div>
-      <div class="p-4 pl-4 bg-slate-900 opacity-70 rounded-lg">
-        <div class="text-bold text-white">Development Archive</div>
+      <div class="p-4 pl-4 m-2 mr-0 bg-gray-300 opacity-20 rounded-lg">
+        <div class="text-bold text-gray">Development Archive</div>
       </div>
     </div>
   </nav>
