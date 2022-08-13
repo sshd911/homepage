2b3bfa34eb60f524854163c8a02f2903fe483312
Sho Maeda <syou20020911@gmail.com>
2022-08-13 10:24:18 +0900

    fix

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 	<head>
 		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>homepage</title>
 		<script src="https://cdn.tailwindcss.com"></script>
+		<script defer src="https://unpkg.com/alpinejs@3.10.3/dist/cdn.min.js"></script>
     <script src="https://unpkg.com/three@0.139.2/build/three.min.js"></script>
     <script src="https://unpkg.com/three@0.139.2/examples/js/controls/OrbitControls.js"></script>
     <script src="https://unpkg.com/three@0.139.2/examples/js/loaders/GLTFLoader.js"></script>
 	</head>
 	<body class="bg-zinc-900">
-		<!-- <body> -->
+		<?php require_once('./src/header.php'); ?>
 		<div id="parentCanvas" class="mt-8 mr-8 container mx-auto">
 			<div id="childCanvas" class="pr-8">
 				<script src="js/three.js"></script>
 			</div>
 		</div>
+		<?php require_once('./src/footer.php'); ?>
 	</body>
 </html>
---
+++ src/header.php
@@ -0,0 +1,17 @@
+<header>
+  <nav class="container flex flex-wrap justify-between items-center mx-auto">
+    <div class="text-bold text-white">Maeda Sho</div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div class="flex flex-wrap justify-between items-center ">
+      <div class="p-4 pr-4 text-bold text-white">
+      About me
+      <div class="bg-gray-100 opacity-10 rounded-lg"></div>
+      </div>
+      <div class="p-4 pl-4 bg-slate-900 opacity-70 rounded-lg">
+        <div class="text-bold text-white">Development Archive</div>
+      </div>
+    </div>
+  </nav>
+</header>
