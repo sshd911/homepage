44731db55d8ad67c10e52a325e1b6bca31e28b76
Sho Maeda <syou20020911@gmail.com>
2022-08-14 14:36:43 +0900

    fix

--- src/header.php
+++ src/header.php
@@ -4,13 +4,13 @@
       <div class="text-bold text-white sm:hidden">Maeda Sho</div>
     </div>
     <div class="flex flex-wrap justify-between items-center width-432:hidden">
-      <div x-on:click="about=true;archives=false;credit=false;" class="p-4 pr-4 m-2 bg-gray-300 opacity-20 rounded-lg">
+      <div x-on:click="about=true;archives=false;credit=false;" class="p-4 pr-4 m-2 bg-gray-300 opacity-20 rounded-lg hover:opacity-30" :class="about && 'opacity-40'">
         <div class="text-bold text-gray">About</div>
       </div>
-      <div x-on:click ="archives=true;about=false;credit=false;" class="p-4 pl-4 m-2 bg-gray-300 opacity-20 rounded-lg">
+      <div x-on:click ="archives=true;about=false;credit=false;" class="p-4 pl-4 m-2 bg-gray-300 opacity-20 rounded-lg hover:opacity-30" :class="archives && 'opacity-40'">
         <div class="text-bold text-gray">Archives</div>
       </div>
-      <div x-on:click ="credit=true;about=false;archives=false" class="p-4 pl-4 m-2 mr-0 bg-gray-300 opacity-20 rounded-lg">
+      <div x-on:click ="credit=true;about=false;archives=false" class="p-4 pl-4 m-2 bg-gray-300 opacity-20 rounded-lg hover:opacity-30" :class="credit && 'opacity-40'">
         <div class="text-bold text-gray">Credit</div>
       </div>
     </div>
