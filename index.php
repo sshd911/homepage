501feaeeee436eb1f1367e6f0947cc77b48d4512
Sho Maeda <syou20020911@gmail.com>
2022-08-14 13:32:36 +0900

    fix

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>homepage</title>
-		<script src="https://cdn.tailwindcss.com"></script>
 		<script defer src="https://unpkg.com/alpinejs@3.10.3/dist/cdn.min.js"></script>
     <script src="https://unpkg.com/three@0.139.2/build/three.min.js"></script>
     <script src="https://unpkg.com/three@0.139.2/examples/js/controls/OrbitControls.js"></script>
     <script src="https://unpkg.com/three@0.139.2/examples/js/loaders/GLTFLoader.js"></script>
+		<link href="/dist/output.css" rel="stylesheet">
 	</head>
 	<body class="bg-zinc-900 container mx-auto items-center md:w-1/2 h-1/2 pt-4 pl-8 pr-8 pb-8" x-data="{ about: true, archives: false, credit: false}">
 		<?php require_once('./src/header.php') ?>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.php",
+    "./src/*.php",
+    "./js/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
