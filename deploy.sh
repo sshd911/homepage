5aba4b11534a75c8ded096e0339becdc26f5ff4e
sshd911 <syou20020911@gmail.com>
2022-12-04 15:03:47 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ npm run build
 cd dist
 echo > .nojekyll
 git init
-git checkout -B prod
+git checkout -B master
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:sshd911/homepage.git master:gh-pages
