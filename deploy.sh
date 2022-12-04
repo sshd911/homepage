834488e8467d675cbc07796cc9b2ea61e9ee84ac
sshd911 <syou20020911@gmail.com>
2022-12-04 15:38:34 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,19 @@
-#!/usr/bin/env sh
+# !/usr/bin/env sh
 
 set -e
 npm run build
 cd dist
 echo > .nojekyll
-git init
+branch=${product}
+existed_in_local=$(git branch --list ${branch})
+if [[ -z ${existed_in_local} ]]; then
+  git branch -D product
+  git checkout -b product
+else
+  git checkout -b product
+fi
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:sshd911/homepage.git master:gh-pages
+git push -f git@github.com:sshd911/homepage.git product:gh-pages
+git checkout master
 cd -
