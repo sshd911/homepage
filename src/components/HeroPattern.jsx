dcb6f274bb3566b2148c008fbb461e0dc0bc06e3
sshd911 <syou20020911@gmail.com>
2024-06-23 10:23:40 +0900

    fix layouts

---
+++ src/components/HeroPattern.jsx
@@ -0,0 +1,25 @@
+import { GridPattern } from '@/components/GridPattern'
+
+export function HeroPattern() {
+  return (
+    <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
+      <div className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
+        <div className="absolute inset-0 bg-gradient-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100">
+          <GridPattern
+            width={50}
+            height={50}
+            x={-12}
+            y={4}
+            squares={[
+              [4, 3],
+              [2, 1],
+              [7, 3],
+              [10, 6],
+            ]}
+            className="absolute inset-x-0 inset-y-[-50%] h-[200%] w-full fill-black/40 stroke-black/50 mix-blend-overlay dark:fill-white/2.5 dark:stroke-white/5"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,16 @@
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { HeroPattern } from '@/components/HeroPattern'
 
 export function Layout({ children }) {
   return (
     <>
       <div className="fixed inset-0 flex justify-center sm:px-8">
+      <HeroPattern>
         <div className="flex w-full max-w-7xl lg:px-8">
           <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
         </div>
+      </HeroPattern>
       </div>
       <div className="relative flex w-full flex-col">
         <Header />
